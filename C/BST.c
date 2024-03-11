2eb43ca6dfe135c52c8d7873c58ee7eae29e990f
vemorphose <anonygamer666@gmail.com>
2024-03-11 12:31:22 +0530

    bst finito

--- C/BST.c
+++ C/BST.c
@@ -59,14 +59,220 @@ void printTree(Tree tree){
     printf("\n");
 }
 
-Tree insertIntoBST(Tree tree, Type key){
-    if (!tree)
-        return newTree(tree, key, NULL, NULL); // base case when no node, new node created and its address returned to parent node to assign
-    if (key < tree->key)
-        tree->left = insertIntoBST(tree->left, key); // after recursive call, immediate child gets assigned to tree->left
-    else
-        tree->right = insertIntoBST(tree->right, key); // same logic after recursive call
-    return tree; // returning to assign to parent node
+Tree insertIntoBST(Tree tree, int key, Tree parent){
+	if (!tree) 
+		return newTree(key, NULL, parent, NULL);
+	if (key < tree -> key)
+		tree -> left = insertIntoBST(tree -> left, key, tree);
+	else
+		tree -> right = insertIntoBST(tree -> right, key, tree);
+	return tree;
+}
+
+Tree insertIntoBSTIterative(Tree tree, int key){
+	if (!tree) return newTree(key, NULL, NULL, NULL);
+	Tree current = tree;
+	Tree parent = tree -> parent;
+	while(current){
+		parent = current;
+		if (key < current -> key)
+			current = current -> left;
+		else	
+			current = current -> right;
+	}
+	if (key < parent -> key)
+		parent -> left = newTree(key, NULL, parent, NULL);
+	else
+		parent -> right = newTree(key, NULL, parent, NULL);
+	return tree;
+}
+
+Tree findTreeInBST(Tree tree, int key){
+	if (!tree)
+		return NULL;
+	if (key == tree -> key)
+		return tree;
+	if (key < tree -> key)	
+		return findTreeInBST(tree -> left, key);
+	return findTreeInBST(tree -> right, key);
+}
+
+Tree findTreeInBSTIterative(Tree tree, int key){
+	while (tree){
+		if (key == tree -> key)
+			return tree;
+		if (key < tree -> key)	
+			tree = tree -> left;
+		else	
+			tree = tree -> right;
+	}
+	return NULL;
+}
+
+int isBST(Tree tree){
+	if (!tree) return 1;
+	if (tree -> left)	
+		if (tree -> key < maxKey(tree -> left)) return 0;
+	if (tree -> right)
+		if (tree -> key > minKey(tree -> right)) return 0;
+
+	int x = isBST(tree -> left);
+	int y = isBST(tree -> right);
+	printf("At %d, got: %d & %d\n", tree -> key, x, y);
+	return x && y;
+}
+
+int max(int a, int b){
+	return a > b ? a : b;
+}
+
+int height(Tree tree){
+	if (!tree) return 0;
+	int heightLeft = height(tree -> left);
+	int heightRight = height(tree -> right);
+	printf("At %3d, Left: %3d, Right: %3d\n", tree -> key, heightLeft, heightRight);
+	return max(heightLeft, heightRight) + 1;
+}
+
+int depth(Tree tree){
+	int depth = 0;
+	for (; tree -> parent; tree = tree -> parent)
+		depth++;
+	return depth;
+}
+
+Tree findMinTreeInBST(Tree tree){
+	for (; tree -> left; tree = tree -> left) ;
+	return tree;
+}
+
+Tree findMaxTreeInBST(Tree tree){
+	for (; tree -> right; tree = tree -> right) ;
+	return tree;
+}
+
+int min(int a, int b){
+	return a < b? a : b;
+}
+
+int maxKey(Tree tree){
+	if (!tree) return 0;
+	if (tree -> left == NULL && tree -> right == NULL)
+		return tree -> key;
+	if (!tree -> left)	
+		return max(tree -> key, maxKey(tree -> right));
+	if (!tree -> right)
+		return max(tree -> key, maxKey(tree -> left));
+	int maxLeftRight = max(maxKey(tree -> left), maxKey(tree -> right));
+	return max(tree -> key, maxLeftRight);
+	//OR return max(tree -> key, max(maxKey(tree -> left), maxKey(tree -> right)));
+}
+
+int minKey(Tree tree){
+	if (!tree) return 0;
+	if (tree -> left == NULL && tree -> right == NULL)
+		return tree -> key;
+	if (!tree -> left)	
+		return min(tree -> key, minKey(tree -> right));
+	if (!tree -> right)
+		return min(tree -> key, minKey(tree -> left));
+	int minLeftRight = min(minKey(tree -> left), minKey(tree -> right));
+	return min(tree -> key, minLeftRight);
+	//OR return min(tree -> key, min(minKey(tree -> left), minKey(tree -> right)));
+}
+
+/*
+	Delete tree:
+		1. NULL - do nothing
+		2. Single node (no child / leaf node) - Delete and return NULL
+		3. One child - Child replaces parent
+		4. Two children - Choose inorder predecessor/ successor
+*/
+
+Tree deleteTreeFromBST(Tree tree, int key){
+	if (!tree) {
+        printf("KEY NOT FOUND\n");
+        return NULL;
+    }
+	if (key < tree -> key){
+        printf("At %d, going left to delete %d\n", tree -> key, key);
+		tree -> left = deleteTreeFromBST(tree -> left, key);
+		return tree;
+	}
+	if (key > tree -> key){
+        printf("At %d, going right to delete %d\n", tree -> key, key);
+		tree -> right = deleteTreeFromBST(tree -> right, key);
+		return tree;
+	}
+    // key == tree -> key
+    printf("Found %d\n", tree -> key);
+	if (!tree -> left && !tree -> right){
+        printf("At %d with no children, going to delete %d\n", tree -> key, key);
+		free(tree);
+		return NULL;
+	}
+	if (tree -> left == NULL){
+        printf("At %d with only right child, going to delete %d\n", tree -> key, key);
+		Tree right = tree -> right;
+		right -> parent = tree -> parent;
+		free(tree);
+		return right;
+	}
+    if (tree -> right == NULL){
+        printf("At %d with only left child, going to delete %d\n", tree -> key, key);
+        Tree left = tree -> left;
+        left -> parent = tree -> parent;
+        free(tree);
+        return left;
+	}
+    Tree successor = findMinTreeInBST(tree -> right);
+    printf("At %d with both children, going to delete %d & successor is %d\n", tree -> key, key, successor -> key);
+    tree -> key = successor -> key;
+    tree -> right = deleteTreeFromBST(tree -> right, successor -> key);
+    return tree ;
+}
+
+Tree findSuccessor(Tree tree){
+	if (!tree ) return NULL;
+	if (tree -> right)
+		return findMinTreeInBST(tree -> right);
+	Tree successor = tree -> parent;
+	// while (successor && tree == successor -> right){
+	while (successor && tree -> key > successor -> key){
+		tree = successor;
+		successor = successor -> parent;
+	}
+	return successor;
+}
+
+Tree findPredecessor(Tree tree){
+	if (!tree ) return NULL;
+	if (tree -> left)
+		return findMaxTreeInBST(tree -> left);
+	Tree predecessor = tree -> parent;
+	// while (predecessor && tree == predecessor -> left){
+	while (predecessor && tree -> key < predecessor -> key){
+		tree = predecessor;
+		predecessor = predecessor -> parent;
+	}
+	return predecessor;
+}
+
+int isIdentical(Tree t1, Tree t2){
+	if (!t1 && !t2) return 1;
+	if ((!t1 && t2) || (t1 && !t2)) return 0;
+	if (t1 -> key != t2 -> key) return 0;
+	return isIdentical(t1 -> left, t2 -> left) && isIdentical(t1 -> right, t2 -> right);
+}
+
+Tree clone(Tree tree){
+	if (!tree) return NULL;
+	Tree left = clone(tree -> left);
+	Tree right = clone(tree -> right);
+	Tree t = newTree(tree -> key, left, tree, NULL);
+	if (left) left -> parent = t;
+	if (right) right -> parent = t;
+	return t;
 }
 
 int main() {
@@ -74,7 +280,7 @@ int main() {
     int n = sizeof(keys)/sizeof(keys[0]);
     Tree bst = NULL;
     for(int i = 0; i < n; i++){
-        bst = insertIntoBST(bst, keys[i]);
+        bst = insertIntoBST(bst, keys[i], NULL);
         printTree(bst);
         printf("\n");
     }
