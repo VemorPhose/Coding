35d84d76f11e4e93d3511b050224048afcaeb2e1
vemorphose <anonygamer666@gmail.com>
2024-03-11 11:57:16 +0530

    Revert "readability"
    This reverts commit 5dadf6ddd2c16dff7c751e3dc3c96e5bbbf35fa3.

--- C/SEM2/ass7/3.c
+++ C/SEM2/ass7/3.c
@@ -3,47 +3,45 @@
 #include <stdbool.h>
 
 void sToPoly(int *p, char *s){
-    int flag = 0; // if 0, write to coeff, else write to exp
-    int sign = 0; // 0 --> +, 1 --> -
+    int flag = 0;
+    int sign = 0;
     int coeff = 0;
     int exp = 0;
-    // loop through each char of inputted string
     for(int i = 0, j = 0; s[i] != 0; i++){
         if(s[i] == ' ')
-            continue; // skip whitespaces
-        if(s[i] == '+' || s[i] == '-'){ // on encountering signs, means last elements is finito
-            if(!coeff){ // if coefficient is zero, no need to update array
+            continue;
+        if(s[i] == '+' || s[i] == '-'){
+            if(!coeff){
                 if(s[i] == '+')
-                    sign = 0; // updates sign for next element
+                    sign = 0;
                 else sign = 1;
-                continue; 
+                continue;
             }
-            if(flag == 1 && exp == 0) // eg. 4x
+            if(flag == 1 && exp == 0)
                 exp = 1;
-            if(sign) // if sign is -, subtract coefficient from that index of array
+            if(sign)
                 p[exp] -= coeff;
             else p[exp] += coeff;
-            coeff = flag = exp = 0; // reset all variables for next element
+            coeff = flag = exp = 0;
             if(s[i] == '+')
-                sign = 0; // updates sign for next element
+                sign = 0;
             else sign = 1;
         }
         else if(s[i] == 'x')
-            flag = 1; // switches write mode from coeff to exp on encountering 'x'
+            flag = 1;
         else{
-            if(!flag){ // updates coeff
+            if(!flag){
                 coeff *= 10;
                 coeff += s[i] - '0';
             }
-            else{ // updates exp
+            else{
                 exp *= 10;
                 exp += s[i] - '0';
             }
         }
     }
-    // for the last element to be inserted into array
-    if(!coeff); // if coefficient is zero, no need to update array
-    else if(flag == 1 && exp == 0) // eg. 4x
+    if(!coeff);
+    else if(flag == 1 && exp == 0)
         exp = 1;
     if(sign)
         p[exp] -= coeff;
