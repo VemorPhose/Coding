fb632de78b55eadc6f079ab8b97ff2d595040012
vemorphose <anonygamer666@gmail.com>
2024-07-12 13:14:45 +0530

    CF round, we back

---
+++ E_Novice_s_Mistake.cpp
@@ -0,0 +1,97 @@
+#pragma GCC optimize ("O3")
+#include <bits/stdc++.h>
+using namespace std;
+
+// typedefs
+#define ll              int64_t
+#define ull             uint64_t
+#define Int             int32_t
+#define uint            uint32_t
+#define pii             pair<ll, ll>
+#define vi              vector<ll>
+#define vvi             vector<vector<ll>>
+#define mpii            map<ll, ll>
+#define umpii           unordered_map<ll, ll>
+#define vs              vector<string>
+
+// shortcuts
+#define MOD             1000000007
+#define mod             998244353
+#define INF             1000000000000000010
+#define PI              2 * acos(0.0)
+#define LL_MAX          LLONG_MAX
+#define LL_MIN          LLONG_MIN
+#define ULL_MAX         ULLONG_MAX
+#define endl            "\n"
+
+// general
+#define fastio()        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define arrIn(arr, n)   for(ll i = 0; i < n; i++) cin >> arr[i]
+#define vIn(v, n)       for(ll i = 0; i < n; i++) { ll temp; cin >> temp; v.pub(temp); }
+#define coutN           cout << "NO" << endl
+#define coutY           cout << "YES" << endl
+
+// binary operations
+#define swap(a,b)       a=a^b; b=b^a; a=a^b
+#define setibit(n,i)    (n | (1<<i))
+#define extibit(n,i)    (n & (1<<i))
+#define clribit(n,i)    (n & ~(1<<i))
+#define invibit(n,i)    (n ^ (1<<i))
+#define setbits(x)      __builtin_popcountll(x)
+#define lzbits(x)       __builtin_clzll(x)
+#define tzbits(x)       __builtin_ctzll(x)
+
+// pairs
+#define ff              first
+#define ss              second
+#define mp              make_pair
+
+// iterable operations
+#define pub             push_back
+#define pob             pop_back
+#define ft              front()
+#define bk              back()
+#define ins             insert
+#define sz(x)           (ll)x.size()
+#define all(x)          (x).begin(), (x).end()
+
+// looping
+#define FOR(i, a, b)    for (int i = (a); i < (b); ++i)
+#define F0R(i, a)       FOR(i, 0, a)
+#define ROF(i, a, b)    for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a)       ROF(i, 0, a)
+
+int main(){
+    fastio();
+    ll t; cin >> t;
+    while(t--){
+        string s; cin >> s;
+        int n = stoi(s);
+        vector<pair<int, int>> vpi;
+        for(int a = 1; a <= 10000; a++){
+            int c = min(10000, a * n);
+            int slen = a * sz(s);
+            string stemp = "";
+            // for(int i = 0; i < a; i++) stemp += s;
+            for(int b = max(a, slen - 10); b <= c; b++){
+                if (!(slen - b > 0 && slen - b < 11)) break;
+                string out = stemp.substr(0, 10);
+                int outint = a * n - b;
+                // int outconv = stoi(out);
+                // if(outint == outconv){
+                //     pair<int, int> p;
+                //     p.first = a;
+                //     p.second = b;
+                //     vpi.pub(p);
+                // }
+            }
+            cout << sz(vpi) << endl;
+            if(sz(vpi)){
+                for(auto p: vpi){
+                    cout << p.first << " " << p.second << endl;
+                }
+            }
+        }
+    }
+    return 0;
+}
