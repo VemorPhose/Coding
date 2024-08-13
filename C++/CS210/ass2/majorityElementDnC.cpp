722d83be8cd627cc35b8b96639f335805dfe920b
vemorphose <anonygamer666@gmail.com>
2024-08-13 16:53:16 +0530

    algo lab

---
+++ C++/CS210/ass2/majorityElementDnC.cpp
@@ -0,0 +1,138 @@
+#pragma GCC optimize ("O3")
+#include <bits/stdc++.h>
+using namespace std;
+
+// typedefs
+#define ll              int64_t
+#define ull             uint64_t
+#define Int             int32_t
+#define uint            uint32_t
+#define pii             pair<ll, ll>
+#define vi              vector<ll>
+#define vvi             vector<vector<ll>>
+#define mpii            map<ll, ll>
+#define umpii           unordered_map<ll, ll>
+#define vs              vector<string>
+
+// shortcuts
+#define MOD             1000000007
+#define mod             998244353
+#define INF             1000000000000000010
+#define PI              2 * acos(0.0)
+#define LL_MAX          LLONG_MAX
+#define LL_MIN          LLONG_MIN
+#define ULL_MAX         ULLONG_MAX
+#define endl            "\n"
+
+// binary operations
+#define swap(a,b)       a=a^b; b=b^a; a=a^b
+#define setibit(n,i)    (n | (1<<i))
+#define extibit(n,i)    (n & (1<<i))
+#define clribit(n,i)    (n & ~(1<<i))
+#define invibit(n,i)    (n ^ (1<<i))
+#define setbits(x)      __builtin_popcountll(x)
+#define lzbits(x)       __builtin_clzll(x)
+#define tzbits(x)       __builtin_ctzll(x)
+
+// pairs
+#define ff              first
+#define ss              second
+#define mp              make_pair
+
+// iterable operations
+#define pub             push_back
+#define pob             pop_back
+#define ft              front()
+#define bk              back()
+#define ins             insert
+#define sz(x)           (ll)x.size()
+#define all(x)          (x).begin(), (x).end()
+
+// looping
+#define FOR(i, a, b)    for (int i = (a); i < (b); ++i)
+#define F0R(i, a)       FOR(i, 0, a)
+#define ROF(i, a, b)    for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a)       ROF(i, 0, a)
+
+// general
+#define fastio()        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define arrIn(arr, n)   for(ll i = 0; i < n; i++) cin >> arr[i]
+#define vIn(v, n)       for(ll i = 0; i < n; i++) { ll temp; cin >> temp; v.pub(temp); }
+#define coutN           cout << "NO" << endl
+#define coutY           cout << "YES" << endl
+
+void merge (vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    vector<int> L(n1), R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0;
+    int k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort (vector<int>& arr, int left, int right) {
+    if (left >= right)
+        return;
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
+}
+
+// Boyer - Moore voting algorithm
+int majorityele(vector<int> a){
+    int i=0,cnt=0;
+    int ele;
+    for(i=0;i<a.size();i++){
+        if(cnt==0){
+            cnt=1;
+            ele=a[i];}
+        else if(a[i]==ele) cnt++;
+        else cnt--;
+    }
+    int cnt_1=0;
+    for(i=0;i<a.size();i++){
+        if(a[i]==ele)
+            cnt_1++;
+        }
+    if(cnt_1>(a.size()/2)) return ele;
+    return -1;
+}
+
+int main(){
+    fastio();
+    int n; cin >> n;
+    vector<int> nums; vIn(nums, n);
+    // mergeSort(nums, 0, n-1);
+    // cout << nums[n/2] << endl;
+
+    cout << majorityele(nums) << endl;
+
+    return 0;
+}
